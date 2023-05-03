dae5dcad76fe60b48e8031c9032962f005b8a6cc
Prabin <prb977@gmail.com>
2023-05-03 19:46:23 -0400

    Fix generate hashes

--- generate_hashes.py
+++ generate_hashes.py
@@ -7,6 +7,24 @@ import stanza
 import networkx as nx
 from stanza.utils.conll import CoNLL
 from conllu import parse_tree_incr
+from transformers import pipeline
+
+
+detoxify_pipeline = pipeline(
+    'text-classification',
+    model='unitary/toxic-bert',
+    tokenizer='bert-base-uncased',
+    function_to_apply='sigmoid',
+    return_all_scores=True
+)
+
+
+def get_toxicity_measures(stories):
+    toxicty_measure = []
+    for s in stories:
+        a = detoxify_pipeline(s.split('.'))
+        toxicty_measure.extend(a)
+    return toxicty_measure
 
 
 nlp = stanza.Pipeline('en')
@@ -107,7 +125,8 @@ if __name__ == '__main__':
     original_stories = list(set(all_stories_opt.text.to_list()))
 
     # Get Sentence Length and hash of original stories
-    sen_lengths, hashes = get_hashes(original_stories[0:1])
+    sen_lengths, hashes = get_hashes(original_stories)
+    toxic_measures = get_toxicity_measures(original_stories)
 
     json.dump(
         sen_lengths,
@@ -119,6 +138,11 @@ if __name__ == '__main__':
         open('hashes/original.json', 'w+')
     )
 
+    json.dump(
+        toxic_measures,
+        open('toxic_measures/original.json', 'w+')
+    )
+
     # OPT
     p_lengths = list(set(all_stories_opt.p_length.to_list()))
     for each in p_lengths:
@@ -127,6 +151,7 @@ if __name__ == '__main__':
         ].gen_text.to_list()
 
         sen_lengths, hashes = get_hashes(stories)
+        toxic_measures = get_toxicity_measures(stories)
 
         json.dump(
             sen_lengths,
@@ -138,6 +163,11 @@ if __name__ == '__main__':
             open(f'hashes/opt_{each}.json', 'w+')
         )
 
+        json.dump(
+            toxic_measures,
+            open(f'toxic_measures/opt_{each}.json', 'w+')
+        )
+
     # LLaMA
     p_lengths = list(set(all_stories_llama.p_length.to_list()))
     for each in p_lengths:
@@ -146,6 +176,7 @@ if __name__ == '__main__':
         ].gen_text.to_list()
 
         sen_lengths, hashes = get_hashes(stories)
+        toxic_measures = get_toxicity_measures(stories)
 
         json.dump(
             sen_lengths,
@@ -157,6 +188,11 @@ if __name__ == '__main__':
             open(f'hashes/llama_{each}.json', 'w+')
         )
 
+        json.dump(
+            toxic_measures,
+            open(f'toxic_measures/llama_{each}.json', 'w+')
+        )
+
     # Alpaca title fixed
     t_types = set(all_stories_alpaca.t_type.to_list())
 
@@ -166,6 +202,7 @@ if __name__ == '__main__':
         ].gen_text.to_list()
 
         sen_lengths, hashes = get_hashes(stories)
+        toxic_measures = get_toxicity_measures(stories)
 
         json.dump(
             sen_lengths,
@@ -177,6 +214,11 @@ if __name__ == '__main__':
             open(f'hashes/alpaca_{each}.json', 'w+')
         )
 
+        json.dump(
+            toxic_measures,
+            open(f'toxic_measures/alpaca_{each}.json', 'w+')
+        )
+
     # Alpaca free
     t_types = set(free_stories_alpaca.t_type.to_list())
 
@@ -186,6 +228,7 @@ if __name__ == '__main__':
         ].gen_text.to_list()
 
         sen_lengths, hashes = get_hashes(stories)
+        toxic_measures = get_toxicity_measures(stories)
 
         json.dump(
             sen_lengths,
@@ -195,4 +238,9 @@ if __name__ == '__main__':
         json.dump(
             hashes,
             open(f'hashes/alpaca_free_{each}.json', 'w+')
+        )
+
+        json.dump(
+            sen_lengths,
+            open(f'toxic_measures/alpaca_free_{each}.json', 'w+')
         )
